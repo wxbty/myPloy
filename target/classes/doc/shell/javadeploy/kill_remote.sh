e7bddac48b0037abac013fa639157cf2a1ba5875
wangxb <wangxb@tuya.com>
2018-06-29 19:20:34 +0800

    add

--- target/classes/doc/shell/javadeploy/kill_remote.sh
+++ target/classes/doc/shell/javadeploy/kill_remote.sh
@@ -6,5 +6,5 @@
 # $2 UUID
 
 echo "kill_remote,remote_ip=$1,uuid=$2";
-ssh root@$1 "ps -aux|grep $2|grep -v grep|cut -c 9-15|xargs kill -9"
+#ssh root@$1 "ps -aux|grep $2|grep -v grep|cut -c 9-15|xargs kill -9"
 echo "服务已停止"
