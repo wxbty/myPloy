3f7b3a1da140c68e7226669adfa4f01e14031963
wangxb <wangxb@tuya.com>
2018-07-03 14:19:23 +0800

    add

--- src/main/java/com/xxg/jdeploy/controller/JavaDeployController.java
+++ src/main/java/com/xxg/jdeploy/controller/JavaDeployController.java
@@ -61,7 +61,7 @@ public class JavaDeployController {
 	 */
 	@RequestMapping(value = "detail/{uuid}", method = RequestMethod.GET)
 	public ModelAndView detail(@PathVariable String uuid) {
-		System.out.println("test update sh");
+		System.out.println("test update sh 2018-07");
 		ModelAndView mv = new ModelAndView("javadeploy/detail");
 		mv.addObject("detail", javaDeployService.getDetail(uuid));
 		return mv;
